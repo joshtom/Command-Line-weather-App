eeaa9580947c5a6916f6caae9a404d82dc80723a
Joshua <olajidejoshua4real@gmail.com>
2019-06-19 18:24:01 +0100

    Api key added from openweathermap.org

--- app.js
+++ app.js
@@ -3,3 +3,24 @@
  * Console.log the value of what you get
  */
 // https://api.openweathermap.org/data/2.5/weather?q=Ibadan&appid=89ba91749d1b7afd202772683a67f2f0
+
+const https = require('https');
+    https.get(`https://api.openweathermap.org/data/2.5/weather?q=Ibadan&appid=89ba91749d1b7afd202772683a67f2f0`, (res) =>{
+        console.log('statusCode:', res.statusCode);
+        console.log('headers', res.headers);
+
+       if(res.statusCode === 200) {
+           let body = "";
+        res.on('data', (data) => {
+            body =  process.stdout.write(data);
+            // body += data.toString();
+        })
+       }
+    });
+
+    
+// Get Weather Information By city name
+
+
+//Get Weather information By city ID
+
