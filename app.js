fca2eb297da2a1b5148401338ce7db699fb97938
Joshua <olajidejoshua4real@gmail.com>
2019-06-19 17:01:48 +0100

    Local Repo initialized

---
+++ app.js
@@ -0,0 +1,5 @@
+/**
+ * Sign up with weather underground to get thier api key
+ * Console.log the value of what you get
+ */
+// https://api.openweathermap.org/data/2.5/weather?q=Ibadan&appid=89ba91749d1b7afd202772683a67f2f0
