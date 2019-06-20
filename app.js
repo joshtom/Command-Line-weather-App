cdab81fa3917f0dbf9baa6a6377b7b6bc355de22
Joshua <olajidejoshua4real@gmail.com>
2019-06-20 13:20:34 +0100

    Static cityName removed and get cityName function added

--- app.js
+++ app.js
@@ -14,34 +14,37 @@ const api = require('./api.json');
     function printMessage(cityName, getWeatherTemp){
         console.log(`The weather Temperature for ${cityName} is ${getWeatherTemp} Degree Celcius`);
     }
-    let cityName = "London";
-    https.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api.key}`, (res) =>{
-        // console.log('statusCode:', res.statusCode);
-        // console.log('headers', res.headers);
-
-       if(res.statusCode === 200) {
-           let body = "";
-        res.on('data', (data) => {
-            // body =  process.stdout.write(data);
-            body += data.toString();
+    
+    // Getting the dynamic input entered by user
+    function get(cityName) {
+        https.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api.key}`, (res) =>{
+            // console.log('statusCode:', res.statusCode);
+            // console.log('headers', res.headers);
+    
+           if(res.statusCode === 200) {
+               let body = "";
+            res.on('data', (data) => {
+                // body =  process.stdout.write(data);
+                body += data.toString();
+            });
+            res.on('end', () =>{
+                try{
+                    const weatherInfo = JSON.parse(body);
+                    let getWeatherTemp = weatherInfo.main.temp;
+                    // console.log(`${getWeatherTemp} Degree Celcius`);
+                    printMessage(cityName, getWeatherTemp);
+                } catch(error) {
+                    console.log(error.message);
+                }
+            })
+           } else {
+               const message = `There was an error getting the weather information for ${cityName} (${http.STATUS_CODES[res.statusCode]})`;
+               const statusCodeError = new Error(message);
+               printError(statusCodeError);
+           }
         });
-        res.on('end', () =>{
-            try{
-                const weatherInfo = JSON.parse(body);
-                let getWeatherTemp = weatherInfo.main.temp;
-                // console.log(`${getWeatherTemp} Degree Celcius`);
-                printMessage(cityName, getWeatherTemp);
-            } catch(error) {
-                console.log(error.message);
-            }
-        })
-       } else {
-           const message = `There was an error getting the weather information for ${cityName} (${http.STATUS_CODES[res.statusCode]})`;
-           const statusCodeError = new Error(message);
-           printError(statusCodeError);
-       }
-    });
-
+    
+    }
     
 // Get Weather Information By city name
 
