237d8931aee98eb1a708294d5383885433b275b7
Joshua <olajidejoshua4real@gmail.com>
2019-06-21 11:32:04 +0100

    Update weather Js

--- weather.js
+++ weather.js
@@ -2,17 +2,16 @@
 //     "coord":{"lon":-0.13,"lat":51.51},"weather":[{"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d"}],"base":"stations","main":{"temp":280.32,"pressure":1012,"humidity":81,"temp_min":279.15,"temp_max":281.15},"visibility":10000,"wind":{"speed":4.1,"deg":80},"clouds":{"all":90},"dt":1485789600,"sys":{"type":1,"id":5091,"message":0.0103,"country":"GB","sunrise":1485762037,"sunset":1485794875},"id":2643743,"name":"London","cod":200
 
 // };
-
-// let getWeatherId = Weather.weather[0].description;
-// let getWeatherTemp = Weather.main.temp;
-// console.log(getWeatherTemp+' Degree Celcius');
+$cityName = "Ibadan";
 const https = require('https');
 const http = require('http');
 const api = require('./api.json');
 
 function printError(error){
-    console.log(error);
+    console.log(error.message);;
 }
+
+
 function printMessage(cityName, getWeatherTemp){
     console.log(`The weather Temperature for ${cityName} is ${getWeatherTemp} Degree Celcius`);
 }
@@ -36,16 +35,19 @@ function get(cityName) {
                 // console.log(`${getWeatherTemp} Degree Celcius`);
                 printMessage(cityName, getWeatherTemp);
             } catch(error) {
+                // printError(error);
                 console.log(error.message);
             }
-        })
+        });
        } else {
            const message = `There was an error getting the weather information for ${cityName} (${http.STATUS_CODES[res.statusCode]})`;
            const statusCodeError = new Error(message);
-           printError(statusCodeError);
+        //    printError(statusCodeError);
+        console.log(statusCodeError.message);
        }
     });
 
 }
 
-module.exports.get = get;
+// module.exports.get = get;
+get($cityName);
